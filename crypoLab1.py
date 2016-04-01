12063d7554aa328e9895ece34ae85ce523252aae
Cory McDonald <cory@corywmconald.com>
2016-04-01 18:52:59 -0500

    Add print statements

--- crypoLab1.py
+++ crypoLab1.py
@@ -28,7 +28,7 @@ serverName = 'localhost'
 sharedSecret = 'DC83C6A952B5D52A9E57FDAB05BE8D085BD0197862399DAE763BB2C898B8AF45'
 
 # provide the port number
-serverPort = 12345
+serverPort = 55555
 
 g = 7
 prime = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C93402849236C3FAB4D27C7026C1D4DCB2602646DEC9751E763DBA37BDF8FF9406AD9E530EE5DB382F413001AEB06A53ED9027D831179727B0865A8918DA3EDBEBCF9B14ED44CE6CBACED4BB1BDB7F1447E6CC254B332051512BD7AF426FB8F401378CD2BF5983CA01C64B92ECF032EA15D1721D03F482D7CE6E74FEF6D55E702F46980C82B5A84031900B1C9E59E7C97FBEC7E8F323A97A7E36CC88BE0F1D45B7FF585AC54BD407B22B4154AACC8F6D7EBF48E1D814CC5ED20F8037E0A79715EEF29BE32806A1D58BB7C5DA76F550AA3D8A1FBFF0EB19CCB1A313D55CDA56C9EC2EF29632387FE8D76E3C0468043E8F663F4860EE12BF2D5B0B7474D6E694F91E6DCC4024FFFFFFFFFFFFFFFF
@@ -155,59 +155,84 @@ def BobGenerateSecretKeys():
 def Alice():
     global diffieHellmanPrivate
     message = ('a'*2000) 
-    print 'Create first set of keys'
-    secretKeys1 = AliceGenerateSecretKeys()
-    print 'Create second set of keys'
-    secretKeys2 = AliceGenerateSecretKeys()
+    secretKeys1 = BobGenerateSecretKeys()
+    print 'Keys created are'
+    print secretKeys1
+    print ''
+    secretKeys2 = BobGenerateSecretKeys()
+    print 'Keys created are'
+    print secretKeys1
+    print ''
 
-    print 'Create AES message'
     aesAliceToBobCipher = AES.new(str(secretKeys1[0]))
     h = SHA256.new()
     h.update(str(secretKeys1[1]))
     aesBobToAliceCipher = AES.new(h.hexdigest()[0:16])
 
-    print 'Sign message and encrypt'
-    privateKey = rsa.PrivateKey.load_pkcs1(alicePrivateKey,'PEM')
+    print 'Plaintext: '
+    print message
+    print ''
+    privateKey = rsa.PrivateKey.load_pkcs1(alicePrivateKey,'PEM')    
     signature = rsa.sign(message, privateKey, 'SHA-256')
     encryptedMessage = aesAliceToBobCipher.encrypt(message)
     
-    print 'Sending signature and encrypted message to Bob'
+    print 'ciphertext:'
+    print encryptedMessage
+    print ''
+    
     BobMessage = ClientSend([signature, encryptedMessage])
-
+    print 'recieved ciphertext: '
+    print  BobMessage
+    print ''
     decryptedMessage = aesBobToAliceCipher.decrypt(BobMessage[1])
     if(BobMessage[0] == HMACMessage(decryptedMessage,str(secretKeys2[1]))):
-        print 'success'
+        print 'decrypted message: '
         decryptedMessage = decryptedMessage[:-8]
         print decryptedMessage
-        print len(decryptedMessage)
+        print ''
         
 
 def Bob():
     global diffieHellmanPrivate
     message = ('b'*1000) + '1' + '0'*7
 
-    print 'Create first set of keys'
     secretKeys1 = BobGenerateSecretKeys()
-    print 'Create second set of keys'
+    print 'Keys created are'
+    print secretKeys1
+    print ''
     secretKeys2 = BobGenerateSecretKeys()
-    
+    print 'Keys created are'
+    print secretKeys1
+    print ''
+
     aesAliceToBobCipher = AES.new(str(secretKeys1[0]))
     h = SHA256.new()
     h.update(str(secretKeys1[1]))
     aesBobToAliceCipher = AES.new(h.hexdigest()[0:16])
-
-    print 'Generate private keys and stuff'
+    
     privateKey = rsa.PrivateKey.load_pkcs1(bobPrivateKey,'PEM')
     encryptedMessage = aesBobToAliceCipher.encrypt(message)
     hmac = HMACMessage(message, str(secretKeys2[1]))
-    print 'Generate HMAC and send'
+    
+    print 'Plaintext: '
+    print message
+    print ''
+    print 'ciphertext:'
+    print encryptedMessage
+    print ''
+    print 'hmac'
+    print hmac
+    print ''
+
     AliceMessage = Server([hmac, encryptedMessage])
-    print('decrypted alice message')
+    print 'recieved ciphertext: '
+    print  AliceMessage
+    print ''
     aliceSentMessage = aesAliceToBobCipher.decrypt(AliceMessage[1])
     if rsa.verify(aliceSentMessage,AliceMessage[0], rsa.PublicKey.load_pkcs1_openssl_pem(alicePublicKey)):
-        print 'success'
+        print 'decrypted message: '
         print aliceSentMessage
-        print len(aliceSentMessage)
+        print ''
 
 
 def Server(message):
@@ -224,7 +249,6 @@ def Server(message):
         connectionSocket, addr = serverSocket.accept()
         # read a sentence of bytes from socket sent by the client
         sentence = pickle.loads(connectionSocket.recv(8192))
-        # print('Recieved ', sentence)
         # if(isinstance(sentence, list) and len(sentence) == 2):
         #     print('ayy')
         data_string = pickle.dumps(message)
@@ -262,7 +286,6 @@ def ClientSend(sentence):
 
             break
         except socket_error as serr:
-            print 'Retrying...', i+1,'/',5
             time.sleep(1)
         if i == 4:
             print 'Connection to server failed'
